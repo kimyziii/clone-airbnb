3462332aa50b5b57398a4bf897acb9347494b321
sleedev <yeaji105@naver.com>
2023-08-17 23:21:45 +0900

    itemList css 수정

--- src/components/HomeMain/ItemList/Item.js
+++ src/components/HomeMain/ItemList/Item.js
@@ -2,37 +2,39 @@ import React from "react";
 import * as S from "../../../styles/HomeMain/ItemList/Item.style";
 import { useNavigate } from "react-router-dom";
 
-const Item = ({item}) => {
+const Item = ({ item }) => {
   const url = item.img1;
 
   // 클릭 시 상세페이지 이동 기능
   const navigate = useNavigate();
-  const goDetailPage = ({item}) => {
-    navigate('/detailpage', {
+  const goDetailPage = ({ item }) => {
+    navigate("/detailpage", {
       state: {
-      id:item.id,
-      title:item.title,
-      filter: item.filter,
-      place:item.place,
-      price:item.price,
-      maxPeople:item.maxPeople,
-      detailText:item.detailText,
-      host:item.host,
-      img1:item.img1,
-      img2:item.img2,
-      img3:item.img3,
-      img4:item.img4,
-      img5:item.img5,
+        id: item.id,
+        title: item.title,
+        filter: item.filter,
+        place: item.place,
+        price: item.price,
+        maxPeople: item.maxPeople,
+        detailText: item.detailText,
+        host: item.host,
+        img1: item.img1,
+        img2: item.img2,
+        img3: item.img3,
+        img4: item.img4,
+        img5: item.img5,
       },
     });
-  }
+  };
 
-    return (
-    <S.Item onClick={() => goDetailPage({item})}>
+  return (
+    <S.Item onClick={() => goDetailPage({ item })}>
       <S.Image style={{ backgroundImage: `url(${url})` }} />
       <S.Title>{item.title}</S.Title>
       <S.Text>{item.host}</S.Text>
-      <S.Title>{item.price} / 최대 {item.maxPeople} 명</S.Title>
+      <S.Title>
+        {item.price} / 최대 {item.maxPeople} 명
+      </S.Title>
     </S.Item>
   );
 };
